5abb607d3212616a21d08fc5003c6d89ed79f639
OMI786I <sohrawardy1998@gmail.com>
2023-10-16 16:24:30 +0600

    Delete a user from client side and database

--- coffee-store-client/src/components/Users.jsx
+++ coffee-store-client/src/components/Users.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
   const loadedUsers = useLoaderData();
+  const [users, setUsers] = useState(loadedUsers);
+  const handleDelete = (id) => {
+    //make sure user is confirmed delte
+    fetch(`http://localhost:3000/user/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.deletedCount > 0) {
+          alert("deleted successfully");
+
+          //remove user from ui
+
+          const remainingUsers = users.filter((user) => user._id !== id);
+          setUsers(remainingUsers);
+        }
+      });
+  };
   return (
     <div>
       <div className="overflow-x-auto">
@@ -9,7 +28,6 @@ const Users = () => {
           {/* head */}
           <thead>
             <tr>
-              <th></th>
               <th>Email</th>
               <th>Created At</th>
               <th>Action</th>
@@ -17,11 +35,18 @@ const Users = () => {
           </thead>
           <tbody>
             {/* row 1 */}
-            {loadedUsers.map((user) => (
+            {users.map((user) => (
               <tr key={user._id}>
                 <td>{user.email}</td>
                 <td>{user.createdAt}</td>
-                <td>Blue</td>
+                <td>
+                  <button
+                    onClick={() => handleDelete(user._id)}
+                    className="btn btn-primary"
+                  >
+                    Delete
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
